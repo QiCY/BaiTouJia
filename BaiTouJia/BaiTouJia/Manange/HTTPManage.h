2121d1aa40617b7add0b005269e0598dec11abc2
洋葱大丰收 <946873595@qq.com>
2017-10-12 13:14:57 +0800

    qicy
    添加af

--- BaiTouJia/BaiTouJia/Manange/HTTPManage.h
+++ BaiTouJia/BaiTouJia/Manange/HTTPManage.h
@@ -9,5 +9,5 @@
 #import <Foundation/Foundation.h>
 
 @interface HTTPManage : NSObject
-
++(id)shareInstance;
 @end
--- BaiTouJia/BaiTouJia/Manange/HTTPManage.m
+++ BaiTouJia/BaiTouJia/Manange/HTTPManage.m
@@ -7,7 +7,18 @@
 //
 
 #import "HTTPManage.h"
-
+#import "AFNetworking.h"
 @implementation HTTPManage
++(id)shareInstance{
+    static HTTPManage *manage = nil;
+    static dispatch_once_t predicate;
+    
+    dispatch_once(&predicate, ^{
+        manage = [[self alloc]init];
+    });
+    
+    return manage;
+}
+
 
 @end
